b66f14a7b0c8c61ab5839bddbf534f73e6c82b22
HaleyW <180524023+HaleyHalcyon@users.noreply.github.com>
2025-08-14 16:16:55 +0900

    add all images

--- docs/dict.js
+++ docs/dict.js
@@ -1,3 +1,5 @@
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
 const GROUPS = {
   senses: [
     "anan",
@@ -102,6 +104,11 @@ let IKAMA_TASUWI = {};
 let DICT = [];
 
 async function getDictionary() {
+  const list = document.getElementById("words");
+  let plsWait = document.createElement("li");
+  plsWait.innerHTML = "Loading dictionary...";
+  list.replaceChildren(plsWait);
+
   const IKAMA_TASUWI_URL = URL.parse("/ikamatasuwi.tsv", window.location);
   console.log(IKAMA_TASUWI_URL);
   const itText = await fetch(IKAMA_TASUWI_URL).then((res) => res.text());
@@ -117,7 +124,10 @@ async function getDictionary() {
           /`(.+?)`/g,
           '<a href="https://nimi.li/$1" target="_blank" class="tp"><i>$1</i></a>'
         )
-        .replaceAll(/\{(.+?)\}/g, '<a href="#word_$1"><i>$1</i></a>');
+        .replaceAll(
+          /\{(.+?)\}/g,
+          '<a href="#word_$1"><img class="inlineIT" src="./img/_$1.png" loading="lazy"> <i>$1</i></a>'
+        );
       return arr;
     })
   );
@@ -126,6 +136,9 @@ async function getDictionary() {
   const TSV_URL =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTVGXFd17kcvfu__zjshqiV3kW360IclOEfEdWda_K6ZCg4TY6nW2Gwn4_bs1yQeFLwrZI1_xEvSuP0/pub?gid=0&single=true&output=tsv";
   const text = await fetch(TSV_URL).then((res) => res.text());
+  plsWait.innerHTML = "Building dictionary...";
+  await delay(1);
+
   DICT = text.trim().split(reNewline);
   // extract the headers we want
   DICT[0] = DICT[0].split("\t");
@@ -153,54 +166,59 @@ async function getDictionary() {
     if (array[keys.word] == "") {
       return undefined;
     }
+    for (let key of Object.keys(keys)) {
+      if (array[keys[key]] == "-" || array[keys[key]] == "" ) {
+        array[keys[key]] = undefined;
+      }
+    }
     return {
       word: array[keys.word],
       mainPos: array[keys.main_pos],
       def: {
-        main: array[keys.def_main] == "-" ? undefined : array[keys.def_main],
-        noun: array[keys.def_noun] == "-" ? undefined : array[keys.def_noun],
-        verb: array[keys.def_verb] == "-" ? undefined : array[keys.def_verb],
-        mod: array[keys.def_mod] == "-" ? undefined : array[keys.def_mod],
-        antonym: array[keys.antonym] == "" ? undefined : array[keys.antonym],
+        main: array[keys.def_main],
+        noun: array[keys.def_noun],
+        verb: array[keys.def_verb],
+        mod: array[keys.def_mod],
+        antonym: array[keys.antonym],
       },
       etym: {
         word: array[keys.etym_word],
         ipa: array[keys.etym_ipa],
         family: array[keys.etym_family],
-        tokiMa: array[keys.toki_ma] == "-" ? undefined : array[keys.toki_ma],
+        tokiMa: array[keys.toki_ma],
       },
       ikamaTasuwi: ikama_tasuwi,
     };
   });
-  DICT = DICT.filter(el => el !== undefined);
+  DICT = DICT.filter((el) => el !== undefined);
   console.log(DICT);
-  const list = document.getElementById("words");
-  list.replaceChildren();
-  populateList(0, 32);
+  setTimeout(() => {
+    populateList();
+  }, 1);
   return;
 }
 
-function populateList(index, count) {
+function populateList() {
   const list = document.getElementById("words");
-  for (let i = index; i < index + count; i++) {
+  list.replaceChildren();
+  for (let i = 0; i < DICT.length; i++) {
     let el = document.createElement("li");
-    if (i >= DICT.length) {
-      return;
-    }
     el.dataset.word = DICT[i].word;
     if (DICT[i].ikamaTasuwi === undefined) {
       el.classList.add("noIkamaTasuwi");
     }
     el.innerHTML = `
 <div class="wordHead" id="word_${DICT[i].word}">
-  <img src="./img/${DICT[i].word}" loading="lazy">
+  <img src="./img/_${DICT[i].word}.png" loading="lazy">
   <div>
     <strong>${DICT[i].word}</strong>
     <i>“${DICT[i].def.main}”</i>
   </div>
 </div><div class="wordBody">
   <ul class="wordIT">
-    <li><strong style="display: none;">Ikama Tasuwi:</strong> ${DICT[i].ikamaTasuwi || "[not found]"}</li>
+    <li><strong style="display: none;">Ikama Tasuwi:</strong> ${
+      DICT[i].ikamaTasuwi || "[not found]"
+    }</li>
   </ul>
   <ul class="wordDef">
     ${
@@ -220,27 +238,30 @@ function populateList(index, count) {
     }
     ${
       DICT[i].def.antonym
-        ? DICT[i].def.antonym.includes("/")
-          ? "<li><strong>Antonyms:</strong> " +
-            Array.from(
-              DICT[i].def.antonym.split("/"),
-              (v) => `<a href="#word_${v}">${v}</a>`
-            ).join(", ") +
+        ? `<li><strong>Antonym${DICT[i].def.antonym.includes("/") ? "s" : ""}:</strong> ` +
+            Array.from(DICT[i].def.antonym.split("/"), (v) => {
+              return Array.from(
+                v.split(" "),
+                (word) =>
+                  `<a href="#word_${word}"><img class="inlineIT" src="./img/_${word}.png" loading="lazy"> <i>${word}</i></a>`
+              ).join(" ");
+            }).join(", ") +
             "</li>"
-          : `<li><strong>Antonym:</strong> <a href="#word_${DICT[i].def.antonym}">${DICT[i].def.antonym}</a></li>`
         : ""
     }
   </ul>
-  <ul class="wordEtym">
-    <li><strong>From:</strong> ${
+  <ul class="wordEtym">${
+    DICT[i].etym.word !== undefined ?
+    `<li><strong>From:</strong> ${
       DICT[i].etym.ipa
         ? DICT[i].etym.word +
           ' <span class="ipa">' +
           DICT[i].etym.ipa +
           "</span>"
         : DICT[i].etym.word
-    }</li>
-    <li><strong>Family:</strong> ${DICT[i].etym.family || "N/A"}</li>
+    }</li><li><strong>Family:</strong> ${
+      DICT[i].etym.family || "N/A"
+    }</li>` : "<li><strong>A priori</strong></li>"}
     ${
       DICT[i].etym.tokiMa
         ? "<li><strong>Toki Ma:</strong> " + DICT[i].etym.tokiMa + "</li>"
@@ -250,51 +271,51 @@ function populateList(index, count) {
 </div>`;
     list.appendChild(el);
   }
-  setTimeout(() => {
-    populateList(index + count, count);
-  }, 1);
 }
 
 function normalSortElements(a, b) {
   return a.dataset.word.localeCompare(b.dataset.word);
 }
-const LIKANU_ORDER = "ptkcwljmnshaieou";
+const LIKANU_ORDER = "ptkcwljmnshNaieou";
+const CODA_N = /([aeiou])n($|[^aeiou])/g;
 function likanuSortElements(a, b) {
-  return likanuSortRecursive(a.dataset.word, b.dataset.word);
+  return likanuSortRecursive(
+    a.dataset.word.replaceAll(CODA_N, "$1N$2"),
+    b.dataset.word.replaceAll(CODA_N, "$1N$2")
+  );
 }
 function likanuSortRecursive(a, b) {
   if (a === "") {
     if (b === "") {
       return 0;
     }
-    return 1;
-  }
-  if (b === "") {
     return -1;
   }
-  const diff = LIKANU_ORDER.indexOf(a.charAt(0)) - LIKANU_ORDER.indexOf(b.charAt(0))
+  if (b === "") {
+    return 1;
+  }
+  const diff =
+    LIKANU_ORDER.indexOf(a.charAt(0)) - LIKANU_ORDER.indexOf(b.charAt(0));
   if (diff === 0) {
-    return likanuSortRecursive(
-      a.substring(1), b.substring(1)
-    );
+    return likanuSortRecursive(a.substring(1), b.substring(1));
   } else {
     return Math.sign(diff);
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  const list = document.getElementById("words");
   getDictionary();
   const search = document.getElementById("wordsSearchBox");
   search.value = "";
-  search.addEventListener("input", event => {
+  search.addEventListener("input", (event) => {
     const v = search.value.trim().toLowerCase();
     if (v === "") {
       for (let child of list.children) {
         child.style.removeProperty("display");
       }
       return;
-    }
-    else {
+    } else {
       for (let child of list.children) {
         if (child.dataset.word.includes(v)) {
           child.style.removeProperty("display");
@@ -304,10 +325,35 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   });
+  search.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+      let searchQuery = search.value.trim().toLowerCase();
+      search.value = "";
+      for (let child of list.children) {
+        child.style.removeProperty("display");
+      }
+      let match = document.getElementById("word_" + searchQuery);
+      if (!match) {
+        let compFunc = sortLikanu.checked
+          ? likanuSortElements
+          : normalSortElements;
+        for (let child of list.children) {
+          if (compFunc(searchQuery, child.dataset.word) < 0) {
+            match = child;
+            break;
+          }
+        }
+      }
+      setTimeout(() => {
+        match.scrollIntoView();
+      }, 10);
+    }
+  });
+
   const sortLikanu = document.getElementById("sortInLikanuOrder");
-  
+
   sortLikanu.checked = false;
-  sortLikanu.addEventListener("change", event => {
+  sortLikanu.addEventListener("change", (event) => {
     let c = Array.from(list.children);
     if (sortLikanu.checked) {
       c.sort(likanuSortElements);
@@ -315,8 +361,6 @@ document.addEventListener("DOMContentLoaded", () => {
       c.sort(normalSortElements);
     }
     console.log(c);
-    list.replaceChildren(
-      ...c
-    );
+    list.replaceChildren(...c);
   });
 });
